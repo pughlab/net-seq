ebdd7c4d4e12f5e962913aff238462fc78803d12
Rene Quevedo <rquevedo@uhnresearch.ca>
2019-05-16 18:43:17 -0400

    Added the beeswarm plots for the correlation plots of GOI with genes that are annotated as core kinetochore genes. (PMID: 24829384)

--- exprSSGSEA/analyzeExtExprs.R
+++ exprSSGSEA/analyzeExtExprs.R
@@ -9,6 +9,7 @@ require(BSgenome.Hsapiens.UCSC.hg19)
 require(matrixStats)
 require(scales)
 
+require(beeswarm)
 ###################
 #### FUNCTIONS ####
 cleanMetadata <- function(gsm, meta.file=NULL, group=NULL){
@@ -53,19 +54,19 @@ cleanMetadata <- function(gsm, meta.file=NULL, group=NULL){
   cl.spl
 }
 
-grpExprMat <- function(gse, cl.spl, group=NULL){
+grpExprMat <- function(gse, cl.spl, group=NULL, return.tmat=FALSE){
   # Clean up multiple probe mappings
-  .cleanExprMat <- function(e.gse, id.df){
+  .cleanExprMat <- function(e.gse, gene.ids){
     # Find genes with multiple probes and reduce them using sample mean
-    mult.genes <- names(which(table(id.df[,2])>1))
+    mult.genes <- names(which(table(gene.ids)>1))
     comb.idx <- sapply(mult.genes, function(i) {
-      grep(paste0("^", i, "$"), x=id.df[,2], ignore.case=TRUE)
+      grep(paste0("^", i, "$"), x=gene.ids, ignore.case=TRUE)
     })
-    na.idx <- which(is.na(id.df[,2]))
+    na.idx <- which(is.na(gene.ids))
     
-    keep.idx <- c(1:nrow(id.df))[-c(na.idx, unlist(comb.idx))]
+    keep.idx <- c(1:nrow(e.gse))[-c(na.idx, unlist(comb.idx))]
     KEEP.e.gse <- e.gse[keep.idx,]
-    rownames(KEEP.e.gse) <- id.df[keep.idx,2]
+    rownames(KEEP.e.gse) <- gene.ids[keep.idx]
     COMB.e.gse <- t(sapply(comb.idx, function(i) colMeans(e.gse[i,])))
     new.e.gse <- rbind(KEEP.e.gse, COMB.e.gse)
     
@@ -75,68 +76,81 @@ grpExprMat <- function(gse, cl.spl, group=NULL){
   e.gse <- exprs(gse)
   if(group=='sadanadam'){
     rownames(e.gse) <- featureData(gse)@data$ORF
+    id.df <- rownames(e.gse)
   } else if(group == 'chan'){
     require("hgu133plus2.db")
     ids <- as.list(hgu133plus2SYMBOL[featureNames(gse)])
-    locs <- as.list(hgu133plus2CHRLOC[featureNames(gse)])
     id.df <- t(sapply(seq_along(ids), function(i) c(names(ids)[i], ids[[i]])))
-    e.gse <- .cleanExprMat(e.gse, id.df)
+    id.df <- id.df[,2]
   } else {
     stop("group must be either 'sadanadam' or 'chan'")
   }
   
-  expr.spl <- lapply(cl.spl, function(grp){
-    if(group == 'chan') g.id = names(grp) else g.id = grp$gsm
-    idx <- which(colnames(e.gse) %in% g.id)
-    
-    e.gse[,idx]
-  })
+  e.gse <- .cleanExprMat(e.gse, id.df)
+  
+  if(return.tmat){
+    expr.spl <- e.gse
+  } else {
+    expr.spl <- lapply(cl.spl, function(grp){
+      if(group == 'chan') g.id = names(grp) else g.id = grp$gsm
+      idx <- which(colnames(e.gse) %in% g.id)
+      
+      e.gse[,idx]
+    })
+  }
   
   expr.spl
 }
 
-compareGoi2Cen <- function(goi, e.gse, expr.spl, gen.plot=FALSE){
-  goi.idx <- grep(goi, rownames(e.gse))
+compareGoi2Cen <- function(goi, e.gse, expr.spl, core.kin, gen.plot=FALSE){
+  goi.idx <- sapply(goi, grep, x=rownames(e.gse))
   goi.exprs <- lapply(expr.spl, function(i) i[goi.idx, ,drop=FALSE])
   
-  # Compare GOI to itself across groups
-  goi.summ <- sapply(goi.exprs, function(i) summary(as.numeric(i)))
-  goi.ttest <- sapply(goi.exprs, function(a) {
-    round(sapply(goi.exprs, function(b) t.test(a,b)$statistic),3)
+  ## Compare GOI to itself across groups
+  goi.ttest <- lapply(goi.exprs, function(a) {
+    lapply(goi.exprs, function(b) {
+      m <- sapply(1:nrow(a), function(gene.a){
+        sapply(1:nrow(b), function(gene.b){
+          t.test(a[gene.a,],b[gene.b,])$statistic
+        })
+      })
+      colnames(m) <- rownames(a); rownames(m) <- rownames(b)
+      m
+    })
   })
   
   ## Check the correlation of expression between GOI and Centromeric proteins
-  cenp.id <- "CENP|MAD2|BUB|KIF2C|AURK"
-  cenp.ids <- rownames(e.gse)[grep(cenp.id, rownames(e.gse), perl=TRUE)]
+  cenp.id <- core.kin[,1]
+  cenp.ids <- rownames(e.gse)[which(rownames(e.gse) %in% cenp.id)]
+  #cenp.id <- "CENP|MAD2|BUB|KIF2C|AURK"
+  #cenp.ids <- rownames(e.gse)[grep(cenp.id, rownames(e.gse), perl=TRUE)]
   cor.mats <- lapply(cenp.ids, function(cenp){
-    cenp.idx <- grep(cenp, rownames(e.gse))
+    cenp.idx <- match(cenp, rownames(e.gse))
     cenp.exprs <- lapply(expr.spl, function(i) i[cenp.idx[1], ,drop=TRUE])
     
     sapply(names(goi.exprs), function(cls){
       if(ncol(goi.exprs[[cls]]) > 2){
-        ct <- cor.test(goi.exprs[[cls]][1,],
-                       cenp.exprs[[cls]], method='pearson')
-        round(c(ct$p.value, ct$estimate),3)
-      } else { c(NA, NA) }
-      
+        sapply(rownames(goi.exprs[[cls]]), function(g){
+          ct <- cor.test(goi.exprs[[cls]][g,],
+                         cenp.exprs[[cls]], method='pearson')
+          #round(c(ct$p.value, ct$estimate),3)
+          round(ct$estimate,3)
+        })
+      } else { NULL }
     })
   })
   names(cor.mats) <- cenp.ids
   
-  cor.pval <- t(sapply(cor.mats, function(i) i[1,]))
-  cor.estimate <- t(sapply(cor.mats, function(i) i[2,]))
-  
   ## Visualization
   if(gen.plot) {
+    if(length(goi) > 1) warning("This was not designed to work with more than 1 GOI")
     boxplot(goi.exprs, ylim=c(0,max(sapply(goi.exprs, max))))
     boxplot(t(sapply(cor.mats, function(i) i[2,])))
   }
   
   list("n"=sapply(expr.spl, ncol),
-       "goi"=list("ttest"=goi.ttest,
-                  "summ"=goi.summ),
-       "cor"=list("pval"=cor.pval,
-                  "estimate"=cor.estimate))
+       "goi"=goi.ttest,
+       "cor"=cor.mats)
 }
 
 genGsc <- function(keyterms, categ='C5', subcat='BP'){
@@ -210,6 +224,47 @@ mapExprsGeneToLoci <- function(e.gse){
   order.idx <- with(anno.expr.genes, order(seqnames, start))
   list("order"=order.idx,
        "anno"=anno.expr.genes[order.idx,])
+}
+
+plotBeeswarmCor <- function(cor.mats, goi, group='core-kinetochore', core.kin){
+  mad <- as.data.frame(sapply(goi, function(g, grp){
+    sapply(cor.mats[['cor']], function(i) i[paste0(g, ".cor"), grp])
+  }, grp='MAD'))
+  
+  wt <- as.data.frame(sapply(goi, function(g, grp){
+    sapply(cor.mats[['cor']], function(i) i[paste0(g, ".cor"), grp])
+  }, grp='WT'))
+  
+  colnames(core.kin) <- c("Symbol", "ID", "group")
+  .annoGeneCor <- function(ds, mad.stat){
+    ds$Symbol <- rownames(ds)
+    ds$mad <- mad.stat
+    merge.ds <- merge(ds, core.kin, by="Symbol", all.x=TRUE)
+    merge.ds$color = 'black'
+    merge.ds[grep("CENP", merge.ds$Symbol),'color'] <- 'red'
+    merge.ds
+  }
+  merged.cor <- rbind(.annoGeneCor(mad, 'MAD'),
+                      .annoGeneCor(wt, 'WT'))
+  ck.merged.cor <- merged.cor[which(merged.cor$group == group),]
+  
+  split.screen(c(1,3))
+  screen(1); par(mar=c(5.1, 4, 4.1, 0.1))
+  beeswarm(DAXX ~ mad, data=ck.merged.cor,breaks=NA, 
+           main='DAXX', ylab='Correlation', xlab='',
+           pwcol=ck.merged.cor$color, pch=16, ylim=c(-0.6, 0.6))
+  screen(2); par(mar=c(5.1, 4, 4.1, 0.1))
+  beeswarm(ATRX ~ mad, data=ck.merged.cor,breaks=NA, 
+           main='ATRX', ylab='', xlab='', yaxt='n',
+           pwcol=ck.merged.cor$color, pch=16, ylim=c(-0.6, 0.6))
+  axis(side = 2, at=seq(-1, 1, by=0.2), labels = rep("", 11))
+  screen(3); par(mar=c(5.1, 4, 4.1, 0.1))
+  beeswarm(MEN1 ~ mad, data=ck.merged.cor,breaks=NA, 
+           main='MEN1', ylab='', xlab='',  yaxt='n',
+           pwcol=ck.merged.cor$color, pch=16, ylim=c(-0.6, 0.6))
+  axis(side = 2, at=seq(-1, 1, by=0.2), labels = rep("", 11))
+  close.screen(all.screens=TRUE)
+  
 }
 
 plotSSGSEA <- function(ssgsea, grp.col){
@@ -289,7 +344,9 @@ plotExprZscore <- function(alt.exprs, ref.exprs,
 ###################
 #### VARIABLES ####
 pdir <- '/mnt/work1/users/pughlab/projects/NET-SEQ/hgu133a2_array_chan/expr_analysis'
-
+core.file <- '/mnt/work1/users/pughlab/projects/NET-SEQ/external_data/thiru_core-kinetochore/supp_E14-03-0837_mc-E14-03-0837-s01.txt'
+core.kin <- read.table(core.file, sep="\t", header=TRUE, 
+                       stringsAsFactors = FALSE, check.names = FALSE)
 
 ###############################
 #### Sadanadam et al, 2015 ####
@@ -304,12 +361,17 @@ gsm <- soft@gsms
 meta.file <- "144026_2_supp_3166825_nvh38t.txt"
 cl.spl <- cleanMetadata(gsm, meta.file, 'sadanadam')
 expr.spl <- grpExprMat(gse, cl.spl, group='sadanadam')
-e.gse <- exprs(gse)
+e.gse <- grpExprMat(gse, cl.spl, group='sadanadam', return.tmat = TRUE)
 
-compareGoi2Cen('DAXX', e.gse, expr.spl, gen.plot=FALSE)
-as.matrix(sort(t(sapply(cor.mats, function(i) i[1,]))[,'MAD']))
-as.matrix(sort(t(sapply(cor.mats, function(i) i[2,]))[,'MAD']))
+## Generate Correlation beeswarm plots highlighting CENP genes
+goi <- c("DAXX", "MEN1", "ATRX")
+cor.mats <- compareGoi2Cen(goi, e.gse, expr.spl[c("MAD", "WT")],
+                           core.kin, gen.plot=FALSE)
+pdf(file.path(pdir, "sad_beeswarmcor.pdf"), width = 6, height = 7)
+plotBeeswarmCor(cor.mats, goi, group='core-kinetochore', core.kin)
+dev.off()
 
+## Generate ssGSEA plots showing disruption of centromeric process
 centromere.gsc <- genGsc(c("centromere", "kinetochore"))
 ssgsea.stats <- grpAndTestSSGSEA(expr.spl[c("MAD", "WT")], centromere.gsc)
 
@@ -317,6 +379,7 @@ pdf(file.path(pdir, "sad_ssgseaMAD.pdf"), width = 6, height = 4)
 plotSSGSEA(ssgsea.stats[['score.test']], grp.col)
 dev.off()
 
+## Generate z-score expression plots showing copy-number
 pdf(file.path(pdir, "sad_exprMAD.pdf"), width = 7, height = 5)
 anno.genes <- mapExprsGeneToLoci(e.gse)
 plotExprZscore(expr.spl[['MAD']], expr.spl[['WT']],
@@ -335,12 +398,17 @@ gsm <- soft@gsms
 
 cl.spl <- cleanMetadata(gsm, group='chan')
 expr.spl <- grpExprMat(gse, cl.spl, group='chan')
-e.gse <- exprs(gse)
+e.gse <- grpExprMat(gse, cl.spl, group='chan', return.tmat = TRUE)
 
-compareGoi2Cen('DAXX', e.gse, expr.spl, gen.plot=FALSE)
-as.matrix(sort(t(sapply(cor.mats, function(i) i[1,]))[,'MAD']))
-as.matrix(sort(t(sapply(cor.mats, function(i) i[2,]))[,'MAD']))
+## Generate Correlation beeswarm plots highlighting CENP genes
+goi <- c("DAXX", "MEN1", "ATRX")
+cor.mats <- compareGoi2Cen(goi, e.gse, expr.spl[c("MAD", "WT")],
+                           core.kin, gen.plot=FALSE)
+pdf(file.path(pdir, "chan_beeswarmcor.pdf"), width = 6, height = 7)
+plotBeeswarmCor(cor.mats, goi, group='core-kinetochore', core.kin)
+dev.off()
 
+## Generate ssGSEA plots showing disruption of centromeric process
 centromere.gsc <- genGsc(c("centromere", "kinetochore"))
 ssgsea.stats <- grpAndTestSSGSEA(expr.spl[c("MAD", "WT")], centromere.gsc)
 
@@ -348,6 +416,7 @@ pdf(file.path(pdir, "chan_ssgseaMAD.pdf"), width = 6, height = 4)
 plotSSGSEA(ssgsea.stats[['score.test']], grp.col)
 dev.off()
 
+## Generate z-score expression plots showing copy-number
 pdf(file.path(pdir, "chan_exprMAD.pdf"), width = 7, height = 5)
 anno.genes <- mapExprsGeneToLoci(e.gse)
 plotExprZscore(expr.spl[['MAD']], expr.spl[['WT']],
